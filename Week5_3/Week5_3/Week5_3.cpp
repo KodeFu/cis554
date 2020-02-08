ba86ab0d4eaf60234f629999b75a5e5e4debfecc
Mudit Vats <muditv@gmail.com>
2020-02-08 16:25:12 -0700

    Update for 5.4 exercise.

--- Week5_3/Week5_3/Week5_3.cpp
+++ Week5_3/Week5_3/Week5_3.cpp
@@ -4,6 +4,8 @@
 ///////////////////////////////////////////////////////////
 
 #include <iostream>
+#include "Maximum.h"
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -44,7 +46,7 @@ int main()
 	int num4;
 	Maximum max;
 
-	cout << "Enter first number: ";
+	/*cout << "Enter first number: ";
 	cin >> num1;
 	cout << "Enter second number: ";
 	cin >> num2;
@@ -52,7 +54,7 @@ int main()
 	cin >> num3;
 
 	cout << "Max Value = (three values) " << max.GetMaximumValue(num1, num2, num3) << endl;
-
+	
 
 	cout << "Enter first number: ";
 	cin >> num1;
@@ -65,6 +67,40 @@ int main()
 
 
 	cout << "Max Value = (four values) " << max.GetMaximumValue(num1, num2, num3, num4) << endl;
+	*/
+
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	cout << "Enter third number: ";
+	cin >> num3;
+	cout << "Enter forth number: ";
+	cin >> num4;
+	cout << "Max Value = (int) " << MaximumValue(num1, num2, num3, num4) << endl;
+
+	double d1, d2, d3, d4;
+	cout << "Enter first number: ";
+	cin >> d1;
+	cout << "Enter second number: ";
+	cin >> d2;
+	cout << "Enter third number: ";
+	cin >> d3;
+	cout << "Enter forth number: ";
+	cin >> d4;
+	cout << "Max Value = (double) " << MaximumValue(d1, d2, d3, d4) << endl;
+
+	char c1, c2, c3, c4;
+	cout << "Enter first number: ";
+	cin >> c1;
+	cout << "Enter second number: ";
+	cin >> c2;
+	cout << "Enter third number: ";
+	cin >> c3;
+	cout << "Enter forth number: ";
+	cin >> c4;
+	cout << "Max Value = (char) " << MaximumValue(c1, c2, c3, c4) << endl;
+
 
 	return 0;
 }
