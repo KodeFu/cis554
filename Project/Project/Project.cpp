9c082c34d7019287b86651360d567a63d495291c
Mudit Vats <muditv@gmail.com>
2020-03-17 06:58:22 -0700

    De-cheese. Loop and ask if you want to quit.

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -76,6 +76,9 @@ int main()
     string playerName;
     string input;
     int playerLevel;
+    bool done = false;
+    bool validInput;
+    int enemyType;
 
     // initialize PRNG
     srand(time(NULL));
@@ -86,109 +89,130 @@ int main()
     // assign refernece to player objects to enemies; could be done 
     // with new/pointer, but using C++ references here
     Player player = Player("Evil Jeff", 1);
-    Dragon dragon = Dragon("Deathwing", 1);
-    Orc orc = Orc("Azog the Terrible", 1);
-    Troll troll = Troll("Grendel", 1);
+    Dragon dragon = Dragon("Doug the Dragon", 1);
+    Orc orc = Orc("Ollie the Orc", 1);
+    Troll troll = Troll("Tommy the Troll", 1);
     enemies.at(0) = &player;
     enemies.at(1) = &dragon;
     enemies.at(2) = &orc;
     enemies.at(3) = &troll;
 
-    cout << "Welcome to Ba-Zork Battle Arena Simulation" << endl << endl;
-    cout << "Where victory (or death) awaits!" << endl;
+    cout << "Welcome to Battle Arena Simulation" << endl << endl;
     cout << endl;
 
-	cout << \
-		"This game allows you to simulate one-on-one battles, D&D style. Attacks and" << endl;
-	cout << \
-		"blocks are random so victory is never a sure thing. You play a Hero who can battle" << endl;
-	cout << \
-		"an Orc, Troll, or, the most vicious, Dragon! Do you have what it takes?" << endl;
+	cout << "This game allows you to simulate one-on-one battles, role-playing style. Attacks and" << endl;
+	cout << "blocks are random so victory is never a sure thing. You play a Hero who can battle" << endl;
+	cout << "a Human, Dragon, Orc, or Troll." << endl;
     cout << endl;
-
-    cout << "What is the hero's name? ";
-    cin >> playerName;
-    cout << endl;
-
-    // get hero level
-    bool validInput = false;
-    do
-    {
-        cout << "What is the hero's level? (1-5) ";
-        cin >> input;
-
-        // check if a valid integer
-        try {
-            playerLevel = std::stoi(input);
-            if (playerLevel > 0 && playerLevel <= 5)
-            {
-                validInput = true;
-            }
-        }
-        catch (...)
-        {
-            cout << "brrrp! ... bad mojo" << endl;
-        }
-
-    } while (!validInput);
-    cout << endl;
-    
-    // Set up hero info
-    Hero hero(playerName, playerLevel);
-
-
-    int enemyType;        
-     
-
-    // get enemy type
-    validInput = false;
-    do
-    {
-        cout << "Who would you like to fight (1=Human, 2=Dragon, 3=Orc, 4=Troll)? ";
-        cin >> input;
-
-        // check if a valid integer
-        try {
-            enemyType = std::stoi(input) - 1;
-            if (enemyType >= 0 && enemyType<4)
-            {
-                validInput = true;
-            }
-        }
-        catch (...)
-        {
-            cout << "brrrp! ... bad mojo" << endl;
-        }
-
-    } while (!validInput);
-    cout << endl;
-
-    // set enemy level based on player level
-    enemies.at(enemyType)->setLevel(playerLevel);
-
-    // start the arena!
-    Arena arena(hero, *enemies.at(enemyType));
-
-    bool dead = false;
-    int turns = 0;
-
-    // show the battle banner; i.e. A vs B
-    arena.showBattleBanner();
 
     do {
-
+        cout << "What is the hero's name? ";
+        cin >> playerName;
         cout << endl;
-        cout << "Turn " << ++turns << endl;
 
-        // battle!
-        dead = arena.doBattle();
+        // get hero level
+        validInput = false;
+        do
+        {
+            cout << "What is the hero's level? (1-5) ";
+            cin >> input;
 
-        // sleep for a second to digest battle text
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            // check if a valid integer
+            try {
+                playerLevel = std::stoi(input);
+                if (playerLevel > 0 && playerLevel <= 5)
+                {
+                    validInput = true;
+                }
+            }
+            catch (...)
+            {
+                cout << "brrrp! ... bad mojo" << endl;
+            }
 
-    } while (!dead);
+        } while (!validInput);
+        cout << endl;
 
-    arena.showVictoryMessage();
+        // Set up hero info
+        Hero hero(playerName, playerLevel);
+
+        // get enemy type
+        validInput = false;
+        do
+        {
+            cout << "Who would you like to fight (1=Human, 2=Dragon, 3=Orc, 4=Troll)? ";
+            cin >> input;
+
+            // check if a valid integer
+            try {
+                enemyType = std::stoi(input) - 1;
+                if (enemyType >= 0 && enemyType < 4)
+                {
+                    validInput = true;
+                }
+            }
+            catch (...)
+            {
+                cout << "brrrp! ... bad mojo" << endl;
+            }
+
+        } while (!validInput);
+        cout << endl;
+
+        // set enemy level based on player level
+        enemies.at(enemyType)->setLevel(playerLevel);
+
+        // start the arena!
+        Arena arena(hero, *enemies.at(enemyType));
+
+        bool dead = false;
+        int turns = 0;
+
+        // show the battle banner; i.e. A vs B
+        arena.showBattleBanner();
+
+        do {
+
+            cout << endl;
+            cout << "Turn " << ++turns << endl;
+
+            // battle!
+            dead = arena.doBattle();
+
+            // sleep for a second to digest battle text
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        } while (!dead);
+
+        // show final battle stats
+        arena.showVictoryMessage();
+
+        // should we play again?
+        string playAgain;
+        validInput = false;
+        do {
+            cout << "Want to play again? (y/n) ";
+            cin >> playAgain;
+            cout << endl;
+
+            if (playAgain=="y")
+            {
+                done = false;
+                validInput = true;
+            }
+            else if (playAgain == "n")
+            {
+                done = true;
+                validInput = true;
+            }
+            else
+            {
+                cout << "brrrp! ... bad mojo" << endl;
+            }
+            
+        } while (!validInput);
+
+    } while (!done);
 }
 
 void showVictoryMessages(Arena* arena)
